db6ad16f5235e329c95935dd4bee158a33f3fea9
Jose <josecarlos.diaz.rayo@gmail.com>
2023-07-11 16:07:03 +0200

    update interfaz with checkbox

--- methods.py
+++ methods.py
@@ -1,11 +1,12 @@
 import random
+
 UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 LOWERCASE_LETTERS = UPPERCASE_LETTERS.lower()
 NUMBERS = "0123456789"
 SIMBOLS = "{|}~[\\]^_`:;<=>?!#$%&\'()*+,-./"
 
 options = (UPPERCASE_LETTERS,LOWERCASE_LETTERS,NUMBERS,SIMBOLS)
-def generate_pass(nick:str,limit:int):
+def generate_pass(nick,limit):
     password = ""
     for i in range(limit):
         password +=random.choice(random.choice(options))  
