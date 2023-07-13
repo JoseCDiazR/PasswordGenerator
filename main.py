964cafe7b064d160aedab495572dc65122fc1b70
Jose <josecarlos.diaz.rayo@gmail.com>
2023-07-13 20:08:06 +0200

    update methods, get data of camps

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
 from methods import generate_pass
 import tkinter
 #Create window and edit
+
+def set_text(entry,text):
+    entry.set(text)
+
+def get_text(entry):
+    return entry.get
+
 window_app = tkinter.Tk()
-window_app.geometry("400x400")
+window_app.geometry("400x350")
 window_app.title("PassGenerator")
 window_app.iconbitmap("key.ico")
-label_nick = tkinter.Label(window_app, text="Nick de registro")
+window_app.resizable(False,False)
+label_nick = tkinter.Label(window_app, text="Registration nick")
 label_nick.place(relwidth=1, relheight=1)
 label_nick.pack()
 textarea_nick = tkinter.Entry(window_app,font="Arial 18")
 textarea_nick.pack()
-
-label_characters = tkinter.Label(window_app, text="Límite de caracteres")
-label_characters.place(relwidth=3, relheight=3)
+textarea_nick.place(x=75,y=30)
+label_characters = tkinter.Label(window_app, text="Character limit")
 label_characters.pack()
-label_characters.place(x=75,y=200)
+label_characters.place(x=160, y=80)
 
 textarea_character = tkinter.Entry(window_app,font="Arial 18")
 textarea_character.pack()
-textarea_character.place(x=75, y=120)
-button_generate = tkinter.Button(window_app, text="Generar", command=None )
-button_generate.pack()
+textarea_character.place(x=75, y=100)
 
 textarea_password = tkinter.Entry(window_app,font="Arial 18")
+textarea_password.configure(state="disabled")
 textarea_password.pack()
-textarea_password.place(x=75,y=300)
+textarea_password.place(x=75,y=240)
+textarea_password.setvar
 
+cb_simbols = tkinter.Checkbutton(window_app,text="Symbols")
+cb_simbols.pack()
+cb_simbols.place(x=75,y=160)
+cb_numbers = tkinter.Checkbutton(window_app,text="Numbers")
+cb_numbers.pack()
+cb_numbers.place(x=75+25*len('Symbols'),y=160)
 
+button_generate = tkinter.Button(window_app, text="Generar", command= "a")
+button_generate.pack()
+button_generate.place(x=180,y=200)
+print(generate_pass("jose",12,True,True))
 window_app.mainloop()
-
-
-"""
-nick = input("Introduce el nick de registro: ")
-limit = int (input("Introduce el límite de caracteres: "))
-
-"""
-print(generate_pass("jose",12))
-
-
-
 
 
 
--- methods.py
+++ methods.py
@@ -3,11 +3,42 @@ import random
 UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 LOWERCASE_LETTERS = UPPERCASE_LETTERS.lower()
 NUMBERS = "0123456789"
-SIMBOLS = "{|}~[\\]^_`:;<=>?!#$%&\'()*+,-./"
+SYMBOLS = "{|}~[\\]^_`:;<=>?!#$%&\'()*+,-./"
+options = (UPPERCASE_LETTERS,LOWERCASE_LETTERS,)
 
-options = (UPPERCASE_LETTERS,LOWERCASE_LETTERS,NUMBERS,SIMBOLS)
-def generate_pass(nick,limit):
-    password = ""
+def generate_pass(nick,limit,selected_numbers,selected_symbols):
+
+    password = list()
+    #Generate pass only with lower and upper case lettes 
     for i in range(limit):
-        password +=random.choice(random.choice(options))  
-    return password 
+        password.append(random.choice(random.choice(options)))
+
+    if nick not in password:
+        if selected_numbers and not selected_symbols :
+            password[random.randint(0, len(password)-1)] = random.choice(NUMBERS)
+            pass      
+        elif selected_symbols and not selected_numbers:
+            password[random.randint(0, len(password)-1)] = random.choice(SYMBOLS)
+            pass
+        elif selected_symbols and  selected_numbers:
+            created_positions = False
+            while not created_positions:
+                position_symbol = random.randint(0, len(password)-1)
+                position_number = random.randint(0, len(password)-1)
+                if position_symbol != position_number:
+                    password[position_symbol] = random.choice(SYMBOLS)
+                    password[position_number] = random.choice(NUMBERS)
+                    break        
+        else:
+            pass
+    return ''.join(password)
+
+def get_data (nick,character,symbols,numbers):
+    nick_data = nick.get()
+    character_data = character.get()
+    symbols_data= True if symbols.get() == 1 else False
+    numbers_data = True if numbers.get() == 1 else False
+
+    generate_pass(nick_data,character_data,symbols_data,numbers_data)
+
+    
