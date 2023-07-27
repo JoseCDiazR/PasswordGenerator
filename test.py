c9db12b927daafea2f407c47af478577b2c815b4
Jose <josecarlos.diaz.rayo@gmail.com>
2023-07-27 14:41:42 +0200

    create test

---
+++ test.py
@@ -0,0 +1,15 @@
+from methods import get_data
+import unittest
+
+
+
+
+
+class TestMetodo(unittest.TestCase):
+    def test_get_data(self):
+        result_1,result_2,result_3,result_4 = get_data("Jose",3,False,True)
+        self.assertEquals(result_1,"Jose")
+        self.assertEquals(result_2,3)
+        self.assertEquals(result_3,0)
+        self.assertEquals(result_4,1)
+
